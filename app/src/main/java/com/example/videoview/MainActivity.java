8a45bf00bef6b32f2434cbead1f97d37038c93fa
Sang Hyun Han <108525941+sanghyunhan0823@users.noreply.github.com>
2022-08-14 02:42:07 +0900

    1st

---
+++ app/src/main/java/com/example/videoview/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.videoview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private VideoView videoView;
+    private MediaController mediaController;
+    private String videoURL = "https://www.w3schools.com/html/mov_bbb.mp4";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        videoView = findViewById(R.id.videoView);
+        mediaController = new MediaController(this);
+
+        mediaController.setAnchorView(videoView);
+        Uri uri = Uri.parse(videoURL);
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(uri);
+        videoView.start();
+
+    }
+}
